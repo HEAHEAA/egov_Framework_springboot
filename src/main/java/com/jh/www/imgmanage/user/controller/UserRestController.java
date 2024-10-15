f90d18f9ff07289228d8f99d130d57e9723679a0
kimji <jhkim@youngjintech.com>
2024-10-15 17:14:27 +0900

    chore: 토큰인증 호출 적용

--- src/main/java/com/jh/www/imgmanage/user/controller/UserRestController.java
+++ src/main/java/com/jh/www/imgmanage/user/controller/UserRestController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/*")
@@ -19,8 +21,16 @@ public class UserRestController {
     JwtTokenProvider jwtTokenProvider;
 
     @GetMapping("user")
-    public ResultModel<Object> selectAllUser(){
+    public ResultModel<Object> selectAllUser(HttpServletRequest request){
+        String access_token = jwtTokenProvider.resolveToken(request);
         ResultModel<Object> resultModel = new ResultModel<>();
+        int auth = jwtTokenProvider.getUserRole(access_token);
+
+        if(auth != 1) {
+            resultModel.setMessage("권한이 없습니다.");
+            resultModel.setSuccess(false);
+            return resultModel;
+        }
         resultModel.setData(userService.selectAllUser());
         resultModel.setMessage("userList success");
         resultModel.setSuccess(true);
@@ -28,8 +38,16 @@ public class UserRestController {
     }
 
     @GetMapping("user/{id}")
-    public ResultModel<Object> selectUserById(@PathVariable("id") String id){
+    public ResultModel<Object> selectUserById(HttpServletRequest request,
+                                              @PathVariable("id") String id){
+        String access_token = jwtTokenProvider.resolveToken(request);
         ResultModel<Object> resultModel = new ResultModel<>();
+        int auth = jwtTokenProvider.getUserRole(access_token);
+        if(auth != 1) {
+            resultModel.setMessage("권한이 없습니다.");
+            resultModel.setSuccess(true);
+            return resultModel;
+        }
         resultModel.setData(userService.selectByIdUser(id));
         resultModel.setSuccess(true);
         resultModel.setMessage("user " + id + "번");
